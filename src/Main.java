30770919faca27ea1bea3331719d2c8b3f3655e6
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-22 03:25:56 -0700

    Stream forEach with explanations

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+
+        List<Integer> nums= Arrays.asList(4,5,7,3,2,6);
+        // asList is a method that help to put values in arrays faster, belongs to the class arrays
+
+//    	for(int i=0;i<nums.size();i++)
+//    	{
+//    		System.out.println(nums.get(i));
+//    	}
+
+
+//    	for(int n: nums)
+//    	{
+//    		System.out.println(n);
+//    	}
+
+        nums.forEach(n -> System.out.println(n)); //Stream API forEach
+        //   forEach(name the variable -> what to do with that variable)
+
+        int sum=0;
+        for(int n:nums)
+        {
+            if(n%2==0)
+            {
+                n=n*2;
+                sum=sum+n;
+            }
         }
+
+//    	System.out.println(nums);
+        System.out.println();
+        System.out.println(sum);
+
     }
 }
